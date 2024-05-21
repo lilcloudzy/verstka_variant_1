94a0a8ab9594f15958116636503f35f479bfb3cd
Nikita Voloshko <83350478+lilcloudzy@users.noreply.github.com>
2024-05-21 14:58:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// включаем музыка на сайте фоном
+
+// window.onload = function () {
+//   var audio = new Audio("Peder B. Helland - Always.mp3");
+//   audio.loop = true;
+//   audio.play();
+// };
+
+// Existing code unchanged.
+// window.onload = function() {
+//     var context = new AudioContext();
+//     // Setup all nodes
+//     // ...
+//   }
+
+//   // One-liner to resume playback when user interacted with the page.
+//   document.querySelector('button').addEventListener('click', function() {
+//     context.resume().then(() => {
+//       console.log('Playback resumed successfully');
+//     });
+//   });
+// https://developer.chrome.com/blog/autoplay?hl=ru
